36c9bdcd4525ee930f3e68a3f248229c34f6e58f
JavaIMMB <electropo4ta2015@yandex.ru>
2018-10-11 10:20:55 +0300

    Spring Boot app + hibernate + security

---
+++ src/main/java/net/javacode/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package net.javacode.sweater.repos;
+
+import net.javacode.sweater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
---
+++ src/main/java/net/javacode/sweater/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package net.javacode.sweater.repos;
+
+import net.javacode.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
