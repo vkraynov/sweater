67abc84cf2dd9a2ed8bfe737cef30dbfebdc9bc3
JavaIMMB <electropo4ta2015@yandex.ru>
2018-10-16 15:20:34 +0300

    @OneToMany + edition of messages

--- src/main/java/net/javacode/sweater/config/WebSecurityConfig.java
+++ src/main/java/net/javacode/sweater/config/WebSecurityConfig.java
@@ -32,16 +32,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
-                .anyRequest().authenticated()
+                    .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
-    }
+                    .rememberMe()
+                .and()
+                    .logout()
+                    .permitAll();
+        }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/net/javacode/sweater/domain/User.java
+++ src/main/java/net/javacode/sweater/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -19,9 +20,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cannot be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct")
@@ -33,6 +31,22 @@ public class User implements UserDetails {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Message> messages;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
@@ -119,11 +133,11 @@ public class User implements UserDetails {
         this.activationCode = activationCode;
     }
 
-    public String getPassword2() {
-        return password2;
+    public Set<Message> getMessages() {
+        return messages;
     }
 
-    public void setPassword2(String password2) {
-        this.password2 = password2;
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
     }
 }
